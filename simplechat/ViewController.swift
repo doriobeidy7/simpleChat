99d271590929bc6b516664261721088bdf885e67
Dori Obeidy <doriobeidy@gmail.com>
2019-04-10 12:26:02 +0300

    Initial Commit

---
+++ simplechat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simplechat
+//
+//  Created by Dori on 4/10/19.
+//  Copyright © 2019 Dori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
